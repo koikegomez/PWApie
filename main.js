b8473a0606a729d53f26b08753e47118f73f2239
Jorge Gallegos <ihexa@outlook.com>
2024-02-08 18:28:36 -0600

    se agrego "res" en main

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator) {
     console.log('Service Worker is supported');
   navigator.serviceWorker.register('/sw.js')
-                            .then(res => console.log('service worker cargo correctamente'))
+                            .then(res => console.log('service worker cargo correctamente',res))
                             .catch(err => console.log('service worker no se ha cargado', err));   
 }else{
 
